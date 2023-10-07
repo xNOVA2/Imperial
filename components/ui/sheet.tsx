27357e99272810ac2c1ba1039a60d63735147d7b
ali usaid <aliusaid55@gmail.com>
2023-10-07 13:40:03 -0700

    Fix sheet Problem

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -14,10 +14,10 @@ const SheetTrigger = SheetPrimitive.Trigger
 const SheetClose = SheetPrimitive.Close
 
 const SheetPortal = ({
-  className,
+
   ...props
 }: SheetPrimitive.DialogPortalProps) => (
-  <SheetPrimitive.Portal className={cn(className)} {...props} />
+  <SheetPrimitive.Portal  {...props} />
 )
 SheetPortal.displayName = SheetPrimitive.Portal.displayName
 
@@ -134,6 +134,8 @@ SheetDescription.displayName = SheetPrimitive.Description.displayName
 
 export {
   Sheet,
+  SheetPortal,
+  SheetOverlay,
   SheetTrigger,
   SheetClose,
   SheetContent,
